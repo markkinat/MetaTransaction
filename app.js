f37a2939eea2d9e76c6f9dbfc6c3cf2952d7d2fb
BanjoSamuelOladele <o.samuel@native.semicolon.africa>
2024-05-06 22:40:49 +0100

    updated

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ABI = require("./TestingAbi.json")
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const { Wallet } = require("ethers");
 
 const app = express();
 
@@ -14,22 +15,28 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("common"));
 
 
+function setUpWallet() {
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+    const encryptedJsonKey = fs.readFileSync("./.encryptedKey.json", "utf8");
+    let wallet = ethers.Wallet.fromEncryptedJsonSync(encryptedJsonKey, process.env.PRIVATE_KEY_PASSWORD);
+    wallet = wallet.connect(provider);
+    return wallet;
+}
+
+function setUpContract(contractAddress, abi, wallet) {
+    return new ethers.Contract(contractAddress, abi, wallet);
+}
+
 
 async function changeBalance(data) {
     try {
-        const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 
-        const encryptedJsonKey = fs.readFileSync("./.encryptedKey.json", "utf8");
-        let wallet = ethers.Wallet.fromEncryptedJsonSync(encryptedJsonKey, process.env.PRIVATE_KEY_PASSWORD);
-        wallet = wallet.connect(provider)
+        let wallet = setUpWallet();
 
-        const contract = new ethers.Contract(process.env.CONTRACT_ADDRESS,
-            ABI,
-            wallet);
+        const contract = setUpContract(process.env.CONTRACT_ADDRESS, ABI, wallet);
         const tx = await contract.changeBalance(data.from, data.amount);
         const receipt = await tx.wait();
         if (receipt.status) {
-
             return { success: true, tx, message: "sent" }
         } else {
             return { success: false, tx, message: "failed" }
@@ -39,7 +46,106 @@ async function changeBalance(data) {
         return {
             success: false, tx: {}, message: error?.reason ?? "ERROR_OCCURED"
         }
+    }
+}
 
+async function createProposal(data) {
+    try {
+
+        let wallet = setUpWallet();
+
+        const contract = setUpContract(process.env.CONTRACT_ADDRESS, ABI, wallet);
+        
+        const tx = await contract.createProposal(data.intiator, data._name, data._deadLine, data.desc);
+        const receipt = await tx.wait();
+
+        if (receipt.status) {
+            return { success: true, tx, message: "sent" }
+        } else {
+            return { success: false, tx, message: "failed" }
+        }
+
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false, tx: {}, message: error?.reason ?? "ERROR_OCCURED"
+        }
+    }
+}
+
+
+async function voteOnProposal(data) {
+    try {
+
+        let wallet = setUpWallet();
+
+        const contract = setUpContract(process.env.CONTRACT_ADDRESS, ABI, wallet);
+
+        const tx = await contract.voteOnProposal(data.intiator, data.proposalId, data.decision, data._tokenId);
+        const receipt = await tx.wait();
+
+        if (receipt.status) {
+            return { success : true, tx, message: "sent" }
+        } else {
+            return { success : false, tx, message: "failed" }
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false, tx: {}, message: error?.reason ?? "ERROR_OCCURED"
+        }
+    }
+}
+
+async function delegateVotingPower(data) {
+    try {
+
+        let wallet = setUpWallet();
+
+        const contract = setUpContract(process.env.CONTRACT_ADDRESS, ABI, wallet);
+
+        const tx = await contract.delegateVotingPower(data.intiator, data._delegate, data._tokenId, data.proposalId);
+        const receipt = await tx.wait();
+
+        if (receipt.status) {
+            return {
+                success: true, tx, message: "sent"
+            }
+        } else {
+            return {
+                success: false, tx, message: "failed"
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        return { success: false, tx: {}, message: error?.reason ?? "ERROR_OCCURED" }
+    }
+}
+
+async function executeProposal(data) {
+    try {
+        
+        let wallet = setUpWallet();
+
+        const contract = setUpContract(process.env.CONTRACT_ADDRESS, ABI, wallet);
+
+        const tx = await contract.executeProposal(data.intiator, data.proposalId);
+        const receipt = await tx.wait();
+
+        if (receipt.status) {
+            return {
+                success: true, tx, message: "sent"
+            }
+        } else {
+            return {
+                success: false, tx, message: 'failed'
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        return {
+            success: false, tx: {}, message: error?.reason ?? "ERROR_OCCURED"
+        }
     }
 }
 
@@ -67,6 +173,87 @@ app.post("/change-balance", async (req, res) => {
         res.status(400).send({ success: false, message: "Invalid signature" })
     }
 })
+
+app.post("/create-proposal", async (req, res) => {
+    const data = req.body;
+    const signerAddress = verifyMessageWithEthers(JSON.stringify({
+        intiator: data.intiator,
+        _name: data.title,
+        _deadLine: data.deadLine,
+        desc: data.description
+    }), data.signature);
+    if (signerAddress.toString()) {
+        const tx = await createProposal(data);
+        if (tx.success) {
+            res.status(200).send(tx);
+        } else {
+            res.status(500).send(tx);
+        }
+    } else {
+        res.status(400).send({ success: false, message: "couldn't verify signature" });
+    }
+});
+
+app.post("/vote-on-proposal", async (req, res) => {
+    const data = req.body;
+    const signerAddress = verifyMessageWithEthers(JSON.stringify({
+        intiator: data.intiator,
+        proposalId: data.proposalId,
+        decision: data.decision,
+        _tokenId: data._tokenId
+    }), data.signature);
+
+    if (signerAddress.toString()) {
+        const tx = await voteOnProposal(data);
+        if (tx.success) {
+            res.status(200).send(tx);
+        } else {
+            res.status(500).send(tx);
+        }
+    } else {
+        res.status(400).send({ success: false, message: "couldn't verify signature" });
+    }
+});
+
+app.post("/delegate-vote", async (req, res) => {
+    const data = req.body;
+    const signerAddress = verifyMessageWithEthers(JSON.stringify({
+        intiator: data.intiator,
+        proposalId: data.proposalId
+    }), data.signature);
+
+    if (signerAddress.toString()) {
+        const tx = await delegateVotingPower(data);
+        if (tx.success) {
+            res.status(200).send(tx);
+        } else {
+            res.status(500).send(tx);
+        }
+    } else {
+        res.status(400).send({ success: false, message: "couldn't verify signature" });
+    }
+});
+
+app.post("/execute-proposal", async (req, res) => {
+    const data = req.body;
+    const signerAddress = verifyMessageWithEthers(JSON.stringify({
+        intiator: data.intiator,
+        _delegate : data._delegate,
+        _tokenId: data._tokenId,
+        proposalId : data.proposalId
+    }), data.signature);
+
+    if (signerAddress.toString()) {
+        const tx = await executeProposal(data);
+        if (tx.success) {
+            res.status(200).send(tx);
+        } else {
+            res.status(500).send(tx);
+        }
+    } else {
+        res.status(400).send({ success: false, message: "couldn't verify signature"});
+    }
+});
 
 app.get("/", async (req, res) => {
     res.send("i am loading...");
